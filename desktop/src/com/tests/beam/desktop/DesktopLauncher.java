47ca243c4d61a8ea861d920aeb4d20c4de498d51
eliotik <eliotik@gmail.com>
2014-04-17 21:52:40 +0300

    init

---
+++ desktop/src/com/tests/beam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tests.beam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tests.beam.Beam;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Beam(), config);
+	}
+}
